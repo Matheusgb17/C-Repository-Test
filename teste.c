f6c61e53226411c14b2c138f777e5352377be4f9
Matheus Garcia <bmatheusgarcia@gmail.com>
2023-08-28 23:35:07 -0300

    Primeiro teste com o git

---
+++ teste.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    system("cls");
+    int num1, num2, res;
+    printf("Hello World\n");
+    
+    printf("Indique um n£mero: ");
+    scanf("%d", &num1);
+    printf("Indique um n£mero: ");
+    scanf("%d", &num2);
+
+    res = num1 + num2;
+
+    printf("Resultado: %d", res);
+
+    if(res > 10){
+        printf("\n\nO Resultado ‚ maior que 10");
+    }
+    else if(res < 10){
+        printf("\n\nO Resultado ‚ menor que 10");
+    }
+    else{
+        printf("\n\nO Resultado ‚ igual a 10");
+    }
+
+    for (int i = res; i >= 0; i--)
+    {
+        printf("\n=> %d ", i);
+    }
+
+    return 0;
+}
